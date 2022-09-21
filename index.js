b959b115f77f45d07601e401ae572af12993d936
Muhammad Zeeshan <muhamadzeeshan03@gmail.com>
2022-09-22 00:18:53 +0500

    cors-issue-resolved

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
+const { request } = require('express')
 const express = require('express')
-
-const mongoose = require('mongoose')
-
+require('./db/config')
+const cors = require('cors')
+const User = require('./db/user')
 const app = express()
 
-const connectDB = async () => {
-  mongoose.connect('mongodb://localhost:27017/e-comm')
-  const productSchema = new mongoose.Schema({})
-  const product = mongoose.model('product', productSchema)
-  const data = await product.find()
-  console.log(data)
-}
+app.use(express.json())
+app.use(cors())
+app.get('/', (req, res) => {
+  res.send('on home page')
+})
+app.post('/register', async (req, res) => {
+  let user = new User(req.body)
+  res.send(req.body)
+  let result = await user.save()
+  res.send(result)
+})
+
 app.listen(5000)
